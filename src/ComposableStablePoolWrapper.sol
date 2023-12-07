7e307fca24d914b50d305d777dcc36f432f64edf
0xLightt <0xlightt@protonmail.com>
2023-12-07 15:45:15 +0000

    Add imports and implement admin collect fee logic

--- src/ComposableStablePoolWrapper.sol
+++ src/ComposableStablePoolWrapper.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.0;
 
 import {FixedPointMathLib} from "solady/utils/FixedPointMathLib.sol";
+import {Ownable} from "solady/auth/Ownable.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 
 import {ERC20, ERC4626, IERC4626} from "@ERC4626/ERC4626.sol";
 
@@ -13,9 +15,18 @@ import {VaultReentrancyLib} from "./lib/VaultReentrancyLib.sol";
 /// @author Maia DAO (https://github.com/Maia-DAO)
 contract ComposableStablePoolWrapper is ERC4626 {
     using FixedPointMathLib for uint256;
+    using SafeTransferLib for address;
     using VaultReentrancyLib for address;
 
-    address vault;
+    /*///////////////////////////////////////////////////////////////
+                            WRAPPER STATE
+    ///////////////////////////////////////////////////////////////*/
+
+    /// @notice The address of the Balancer's vault.
+    address public immutable vault;
+
+    /// @notice The address of the factory that created this wrapper.
+    address public immutable factory;
 
     /**
      * @param _asset the underlying asset
@@ -23,13 +34,39 @@ contract ComposableStablePoolWrapper is ERC4626 {
      * @param _symbol the symbol of the LP
      */
     constructor(ERC20 _asset, string memory _name, string memory _symbol) ERC4626(_asset, _name, _symbol) {
+        factory = msg.sender;
         vault = IComposableStablePool(address(asset)).getVault();
     }
 
+    /*///////////////////////////////////////////////////////////////
+                              ADMIN LOGIC
+    ///////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Collects all fees accumulated in the wrapper and transfers them to the factory owner.
+     * @param _receiver The address to receive the fees.
+     */
+    function collectFees(address _receiver) external {
+        if (factory != msg.sender) revert Unauthorized();
+
+        // Calculate fees to collect (previewMint already ensures we are not in the vault context)
+        // previewMint gets the amount of assets that would be withdrawn if we were to withdraw all shares, but it
+        // rounds up for safety, so we may have a small amount of assets left over
+        uint256 fees = totalAssets() - previewMint(totalSupply);
+
+        // Transfer fees to receiver
+        address(asset).safeTransfer(_receiver, fees);
+
+        emit CollectFees(_receiver, fees);
+    }
+
     /*//////////////////////////////////////////////////////////////
-                            ACCOUNTING LOGIC
+                       ERC4626 ACCOUNTING LOGIC
     //////////////////////////////////////////////////////////////*/
 
+    /**
+     * @notice The total amount of assets held by this wrapper, including fees.
+     */
     function totalAssets() public view override returns (uint256) {
         return asset.balanceOf(address(this));
     }
@@ -37,6 +74,7 @@ contract ComposableStablePoolWrapper is ERC4626 {
     /// @inheritdoc IERC4626
     function convertToShares(uint256 assets) public view override returns (uint256) {
         vault.ensureNotInVaultContext();
+
         // Multiply by rate to get shares, rounding down
         return assets.mulWad(IComposableStablePool(address(asset)).getRate());
     }
@@ -44,6 +82,7 @@ contract ComposableStablePoolWrapper is ERC4626 {
     /// @inheritdoc IERC4626
     function convertToAssets(uint256 shares) public view override returns (uint256) {
         vault.ensureNotInVaultContext();
+
         // Divide by rate to get assets, rounding down
         return shares.divWad(IComposableStablePool(address(asset)).getRate());
     }
@@ -51,6 +90,7 @@ contract ComposableStablePoolWrapper is ERC4626 {
     /// @inheritdoc IERC4626
     function previewMint(uint256 shares) public view override returns (uint256) {
         vault.ensureNotInVaultContext();
+
         // Divide by rate to get assets, rounding up
         return shares.divWadUp(IComposableStablePool(address(asset)).getRate());
     }
@@ -58,7 +98,21 @@ contract ComposableStablePoolWrapper is ERC4626 {
     /// @inheritdoc IERC4626
     function previewWithdraw(uint256 assets) public view override returns (uint256) {
         vault.ensureNotInVaultContext();
+
         // Multiply by rate to get shares, rounding up
         return assets.mulWadUp(IComposableStablePool(address(asset)).getRate());
     }
+
+    /*///////////////////////////////////////////////////////////////
+                                EVENTS
+    ///////////////////////////////////////////////////////////////*/
+
+    event CollectFees(address indexed receiver, uint256 indexed fees);
+
+    /*///////////////////////////////////////////////////////////////
+                                ERRORS
+    ///////////////////////////////////////////////////////////////*/
+
+    /// @notice Throws if called by any account other than the factory.
+    error Unauthorized();
 }
